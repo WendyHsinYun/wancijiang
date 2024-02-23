c0ed7ab70bd1dd9b3a2a6cb3518375ae965741f9
Wendy HsinYun <wendyhsinyun@gmail.com>
2024-02-23 13:06:52 +0800

    remove favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,10 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
       htmlAttrs: {
         lang: 'en'
-      }
+      },
+      link: [
+        { rel: "icon", type: "image/png", href: "" }
+      ]
     }},
 
   devtools: { enabled: false },
